ce6b66dce68f60efd3d2bf29135a0f1a79939bb1
Gargi Sharma <gs051095@gmail.com>
2020-06-08 13:34:32 +0530

    Lists and BSTs

---
+++ lists/lists.ml
@@ -0,0 +1,108 @@
+exception Empty
+
+exception Subscript
+
+module type STACK = sig
+  type 'a stack
+
+  val empty : 'a stack
+
+  val isEmpty : 'a stack -> bool
+
+  val cons : 'a -> 'a stack -> 'a stack
+
+  val head : 'a stack -> 'a
+
+  val tail : 'a stack -> 'a stack
+
+  val ( ++ ) : 'a stack -> 'a stack -> 'a stack
+end
+
+module ListStack : STACK = struct
+  type 'a stack = 'a list
+
+  let empty = []
+
+  let isEmpty s = s = []
+
+  let cons x s = x :: s
+
+  let head = function [] -> raise Empty | h :: _ -> h
+
+  let tail = function [] -> raise Empty | _ :: t -> t
+
+  let rec ( ++ ) xs ys = match xs with [] -> ys | xh :: xt -> xh :: (xt ++ ys)
+end
+
+module CustomStack : STACK = struct
+  type 'a stack = Nil | Cons of 'a * 'a stack
+
+  let empty = Nil
+
+  let isEmpty s = Nil = s
+
+  let cons x s = Cons (x, s)
+
+  let head = function Nil -> raise Empty | Cons (x, _) -> x
+
+  let tail = function Nil -> raise Empty | Cons (_, s) -> s
+
+  let rec ( ++ ) xs ys =
+    if isEmpty xs then ys else cons (head xs) (tail xs ++ ys)
+end
+
+let rec update lst i y =
+  match (lst, y) with
+  | [], _ -> raise Subscript
+  | _ :: xs, 0 -> y :: xs
+  | x :: xs, _ -> x :: update xs (i - 1) y
+
+let rec suffixes xs =
+  match xs with [] -> [ [] ] | hd :: tl -> hd :: suffixes tl
+
+module type SET = sig
+  type elem
+
+  type set
+
+  val empty : set
+
+  val insert : elem -> set -> set
+
+  val member : elem -> set -> bool
+end
+
+module type ORDERED = sig
+  type t
+
+  val eq : t -> t -> bool
+
+  val lt : t -> t -> bool
+
+  val leq : t -> t -> bool
+end
+
+module UnbalancedSet (Element : ORDERED) : SET with type elem = Element.t =
+struct
+  type elem = Element.t
+
+  type tree = E | T of tree * elem * tree
+
+  type set = tree
+
+  let empty = E
+
+  let rec member x = function
+    | E -> false
+    | T (a, y, b) ->
+        if Element.lt x y then member x a
+        else if Element.lt y x then member x b
+        else true
+
+  let rec insert x = function
+    | E -> T (E, x, E)
+    | T (a, y, b) as s ->
+        if Element.lt x y then T (insert x a, y, b)
+        else if Element.lt y x then T (a, y, insert x b)
+        else s
+end
